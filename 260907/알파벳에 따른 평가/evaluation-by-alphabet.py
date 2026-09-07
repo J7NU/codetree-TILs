6bfed018968016fa4f0e5569035404c9b75becd3
J7NU <124552270+J7NU@users.noreply.github.com>
2026-09-07 18:49:14 +0900

    [Passed] 알파벳에 따른 평가 (49ms, 16MB)

---
+++ 260907/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+w = input()
+if w == "S":
+    print("Superior")
+elif w == "A":
+    print("Excellent")
+elif w == "B":
+    print("Good")
+elif w == "C":
+    print("Usually")
+elif w == "D":
+    print("Effort")
+else:
+    print("Failure")
