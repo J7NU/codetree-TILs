dcdb952e1c42978d65020ed4865660cfeafe2d77
J7NU <124552270+J7NU@users.noreply.github.com>
2026-09-08 21:13:01 +0900

    [Passed] 두 사람 (49ms, 16MB)

---
+++ 260908/두 사람/two-person.py
@@ -0,0 +1,7 @@
+o1, a1 = input().split()
+o2, a2 = input().split()
+# print(type(o1),o2,a1,a2)
+if (int(o1) >= 19 and a1 == "M") or (int(o2) >= 19 and a2 == "M"):
+        print(1)
+else:
+    print(0)
